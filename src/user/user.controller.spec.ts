3a00002efc4fb8d91b77961860bfdbeb3405dc8f
marcel0paixao <celomar01@hotmail.com>
2024-07-24 13:24:04 +0000

    user controller tests

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -2,9 +2,26 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { PrismaService } from '../prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { Prisma, User } from '@prisma/client';
 
 describe('UserController', () => {
   let controller: UserController;
+  let service: UserService;
+  let prismaService: PrismaService;
+
+  const createUserDto: CreateUserDto = {
+    name: "test",
+    email: "test@test.test",
+    password: "Test@test"
+  };
+
+  const createdUser: User = {
+    id: 1,
+    name: createUserDto.name,
+    email: createUserDto.email,
+    password: createUserDto.password,
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -13,9 +30,21 @@ describe('UserController', () => {
     }).compile();
 
     controller = module.get<UserController>(UserController);
+    service = module.get<UserService>(UserService);
+    prismaService = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should create user', async () => {
+    jest.spyOn(prismaService.user, 'create').mockResolvedValue(createdUser);
+    
+    const result = await controller.create(createUserDto);
+    expect(result).toEqual({
+      ...createdUser,
+      password: undefined
+    });
   });
 });
